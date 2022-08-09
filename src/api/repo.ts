e152ad0ff3860df7592b8dde40b337a0b8aa5fcb
Aleksandar Zajic <aleksandar.zajic@digitalinsurance.io>
2022-08-09 18:58:50 +0200

    chore: cleanup code

--- src/api/repo.ts
+++ src/api/repo.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
 
-import { Query } from '@/hooks'
+// import { Query, useQuery } from '@/hooks'
 
 const url = 'https://api.github.com/repos/alekzajic'
 const get = (url: string) => fetch(url).then((response) => response.json())
@@ -14,6 +14,7 @@ export type Repo = {
 
 export const getRepo = (name = 'vitality'): Promise<Repo> => get(`${url}/${name}`)
 
+// export const useRepo = (name = 'vitality'): Query<Repo> => {
 export const useRepo = (name = 'vitality') => {
   return useQuery<Repo>(['repo', name], () => getRepo(name), { cacheTime: Infinity })
 }
