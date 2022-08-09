6a9f95c2ba86a6b3b0c743cbb3f3a9a484b1e730
Aleksandar Zajic <aleksandar.zajic@digitalinsurance.io>
2022-08-09 17:48:55 +0200

    feat: added zustand;added appstore

---
+++ src/stores/app.ts
@@ -0,0 +1,23 @@
+import create from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface AppState {
+  currentLocale: string
+  setCurrentLocale: (locale: string) => void
+}
+
+export const useAppStore = create(
+  persist<AppState>(
+    (setState) => ({
+      currentLocale: 'en-GB',
+      setCurrentLocale: (locale) =>
+        setState((state) => ({
+          ...state,
+          currentLocale: locale,
+        })),
+    }),
+    {
+      name: 'app_store',
+    },
+  ),
+)
